dab207dc772d13adec0962c04b61e77b2bab3f7f
Tanmay Khanna <120933239+TomLucasakaTGeek@users.noreply.github.com>
2023-07-31 20:26:49 +0530

    Create for loop.c
    Explanation of for loop

---
+++ Basics/Loops/for loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+  int n;
+  printf("Enter a number");
+  scanf("%d", &n);
+  //for loops are generally used for known number of iterations
+  for(int i=0;i<n;i++)   /* syntax of for 
+    {                       (<1>initialization;<2>range;<4>increment)
+    printf(i);                         <3>Procedure 
+    }                     */
+}
