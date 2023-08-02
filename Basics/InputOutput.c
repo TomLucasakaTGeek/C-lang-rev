bd71f792495d99d2a4a671db2dd630aa472cf546
Tanmay Khanna <120933239+TomLucasakaTGeek@users.noreply.github.com>
2023-08-02 14:12:09 +0530

    Update InputOutput.c

--- Basics/InputOutput.c
+++ Basics/InputOutput.c
@@ -5,7 +5,7 @@ int main()
   //using scanf()
   scanf("%d", &x); //scanf() is used to take input until a whitespace(blank) is encountered  
   //using printf()
-  printf("Value of x is: %d", x); //usually uesd for printing 
+  printf("Value of x is: %d", x); //usually used for printing 
   
   //using getchar()
   c = int getchar(void);  //reads input for single(1) character and converts it into integer
