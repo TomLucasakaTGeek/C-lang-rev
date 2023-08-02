1b82a0aee49f1223c853eaa5860d687e3dcfc26a
Tanmay Khanna <120933239+TomLucasakaTGeek@users.noreply.github.com>
2023-08-02 14:09:11 +0530

    Update do while.c

--- Basics/Loops/do while.c
+++ Basics/Loops/do while.c
@@ -1,1 +1,16 @@
-#inlude<stdio.h>
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number of times you want to print your name:");
+    scanf("%d", &n);
+    char name[25];
+    printf("\nEnter your name:");
+    scanf("%s", name);
+  //do while is used when we want the execute the body of loop before testing the condition
+    do{                               //syntax of do while:
+        printf("%s\n", name);          // do{ <operation> }while(condition);
+        n--;
+    }while(n < 0);
+    return 0;
+}
