b60b02d1d938fe3c8dd460dcf9045651089a2043
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2024-03-07 21:33:40 +0530

    Creating a Stack

--- DSA/Stack/Stcreate.c
+++ DSA/Stack/Stcreate.c
@@ -1,1 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+struct Stack {
+    int data;
+    int top;
+    int *arr;
+};
+
+int isEmpty(struct Stack *ptr){
+    if(ptr->top==-1)
+        return 1;
+    else
+        return 0;
+}
+
+int isFull(struct Stack *ptr){
+    if(ptr->top==(ptr->size)-1)
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+    struct Stack s;
+    s->size = 80;
+    s->top = -1;
+    s->arr = (int*)malloc(s->size * sizeof(int));
+
+    if(isEmpty(s)){
+        printf("Stack is Empty");
+    }
+    else{
+        printf("Stack is not Empty");
+    }
+    return 0;
+}
